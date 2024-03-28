8440a353b587ca14f1c6542aaecdece9c54c9366
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-03-28 19:58:56 +0100

    update on Articles

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,5 +1,6 @@
 import Card from "react-bootstrap/Card";
 import { InterfaceArticle } from "../interfaces/InterfaceArticle";
+import { Col, Container, Row } from "react-bootstrap";
 
 interface ArticleProps {
   articleDetails: InterfaceArticle;
@@ -7,13 +8,19 @@ interface ArticleProps {
 
 const Articles = function (props: ArticleProps) {
   return (
-    <Card className="my-2">
-      <Card.Img src={props.articleDetails.image_url} />
-      <Card.Body>
-        <Card.Title>{props.articleDetails.title}</Card.Title>
-        <Card.Title>{props.articleDetails.news_site}</Card.Title>
-      </Card.Body>
-    </Card>
+    <Container>
+      <Row>
+        <Col>
+          <Card className="my-2">
+            <Card.Img src={props.articleDetails.image_url} />
+            <Card.Body>
+              <Card.Title>{props.articleDetails.title}</Card.Title>
+              <Card.Title>{props.articleDetails.news_site}</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
